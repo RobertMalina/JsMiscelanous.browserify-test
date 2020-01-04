6944b16f11d23476fbccb5dc9983c7f5032a7892
Robert <robert.malina987@gmail.com>
2020-01-04 10:59:01 +0100

    initial public assets add

---
+++ main.js
@@ -0,0 +1,35 @@
+const ItemList = require('./ui/item-list');
+const { createOrderRow } = require('./ui/order-list-item');
+const DataService = require('./service');
+
+const list = new ItemList({
+  selector: '#order-list',
+  createRow: createOrderRow,
+  title: 'Orders list',
+});
+list.create();
+
+const service = new DataService();
+
+const onOrdersLoaded = function(response) {
+  const { orders } = response;
+  list.clearRows();
+  orders.forEach(function(order) {
+    list.addRow(order);
+  });
+};
+
+const onOrdersLoadError = function(response) {};
+
+const loadBtn = document.getElementById('load-list');
+loadBtn.addEventListener('click', function() {
+  service.paggingSupport.getOrders(
+    {
+      page: 1,
+      itemsOnPage: 5,
+      archivedToo: true,
+    },
+    onOrdersLoaded,
+    onOrdersLoadError,
+  );
+});
